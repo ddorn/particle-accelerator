bb0b2eb5b649291d01aa0bf986ed3e9c6f64e5c8
ddorn <diego.dorn@free.fr>
2019-03-17 20:37:26 +0100

    comments in constants

--- header/constants.h
+++ header/constants.h
@@ -1,17 +1,24 @@
-//
-// Created by Gabin on 08.03.2019.
-//
+/**
+ * This header defines the constants namespace,
+ * which has the universal constants useful for
+ * or project.
+ */
 
 #ifndef PARTICLE_ACCELERATOR_CONSTANTS_H
 #define PARTICLE_ACCELERATOR_CONSTANTS_H
 
 namespace constants {
-    constexpr double LIGHT_SPEED(1.0); // speed of light, unit : LIGHT_SPEED
+    /// Speed of light. Unit: c
+    constexpr double LIGHT_SPEED(1.0);
+    /// Speed of light squared. Unit: c²
     constexpr double LIGHT_SPEED_SQUARED(LIGHT_SPEED * LIGHT_SPEED);
 
-    constexpr double PROTON_CHARGE(1.0); //charge of a proton
-    constexpr double M_PROTON(0.938272); // Mass of a proton in GeV/c²
-    constexpr double M_ELECTRON(5.10999e-4); // Mass of an electron in GeV/c²
+    /// Charge of a proton. Unit: eV
+    constexpr double PROTON_CHARGE(1.0);
+    /// Mass of a proton. Unit: GeV/c^2
+    constexpr double M_PROTON(0.938272);
+    /// Mass of an electron. Unit: GeV/c²
+    constexpr double M_ELECTRON(5.10999e-4);
 }
 
 #endif //PARTICLE_ACCELERATOR_CONSTANTS_H
