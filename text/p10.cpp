e50e3ac666dbb1e6fd84bc4a0f39b9560f526af9
ddorn <diego.dorn@free.fr>
2019-03-31 17:37:53 +0200

    created Accelerator for p10

---
+++ text/p10.cpp
@@ -0,0 +1,41 @@
+//
+// Created by diego on 3/31/19.
+//
+
+#include <iostream>
+#include "TextSupport.h"
+#include "Accelerator.h"
+
+using namespace std;
+
+int main() {
+    TextSupport s(cout);
+    Accelerator bobLEponge(&s, Vector3D(0, 3, 0));
+
+    double radius(0.1);
+    Vector3D pqf(1, 3, 0);
+    Vector3D pd1(2, 3, 0);
+    Vector3D pd(3, 2, 0);
+    Vector3D pqd(3, 1, 0);
+    Vector3D pd2(3, 0, 0);
+
+    for (int i = 0; i < 4; ++i) {
+        bobLEponge.addQuadruple(pqf, radius, -1.2);
+        bobLEponge.addSegment(pd1, radius);
+        bobLEponge.addDipole(pd, radius, 1, 5.89158);
+        bobLEponge.addQuadruple(pqd, radius, 1.2);
+        bobLEponge.addSegment(pd2, radius);
+
+        pqf = pqf ^ Vector3D::e3;
+        pd1 = pd1 ^ Vector3D::e3;
+        pd = pd ^ Vector3D::e3;
+        pqf = pqd ^ Vector3D::e3;
+        pd2 = pd2 ^ Vector3D::e3;
+    }
+    bobLEponge.draw();
+    for (int j = 0; j < 10; ++j) {
+        bobLEponge.evolve();
+        bobLEponge.draw();
+    }
+    return 0;
+}
