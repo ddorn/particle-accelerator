80980464e3afc5a3b34f580ace5d32303185bd04
Leopold Kolly <gabin1999@hotmail.com>
2019-03-31 11:56:23 +0200

    add segment class

---
+++ header/Segment.h
@@ -0,0 +1,26 @@
+//
+// Created by Gabin on 31.03.2019.
+//
+
+#ifndef PARTICLEACCELERATOR_SEGMENT_H
+#define PARTICLEACCELERATOR_SEGMENT_H
+
+#endif //PARTICLEACCELERATOR_SEGMENT_H
+
+#include "Vector3D.h"
+#include "Particle.h"
+#include "StraightElement.h"
+#include "Element.h"
+
+class Segment : public StraightElement{
+
+public:
+    Segment(const Vector3D &entree, const Vector3D &exit, double radius, Element *nextElement)
+            : StraightElement(entree, exit, radius, nextElement) {}
+    /**
+     * Segment applies no magnetic force
+     * It returns a null vector.
+     */
+    const Vector3D magneticForceAt(const Vector3D & /*position*/) const override { return Vector3D(); }
+    std::ostream& print(std::ostream &os) const override;
+};
