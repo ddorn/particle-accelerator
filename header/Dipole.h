a97be6c5cf861634ff0929c313b32aeaa570ef09
Leopold Kolly <gabin1999@hotmail.com>
2019-05-07 22:32:06 +0200

    add new way to create dipole (calculate the ideal magnetic intensity)

--- header/Dipole.h
+++ header/Dipole.h
@@ -13,6 +13,7 @@ private:
      * magnetic field. Unit : tesla.
      */
     const Vector3D magneticField_;
+    const Vector3D idealMagneticField(double mass, double charge, double energy) const;
 
 public:
     /**
@@ -20,11 +21,18 @@ public:
      * the dipoles can only apply a vertical magnetic field.
      * So the constructor asks a double, not a vector, to assure that.
      */
-    Dipole(const Vector3D &entree, const Vector3D &exit, double radius, Element *nextElement,
+    Dipole(const Vector3D &start, const Vector3D &exit, double radius, Element *nextElement,
            double curvature, double magneticFieldIntensity) :
-            CurvedElement(entree, exit, radius, nextElement, curvature),
+            CurvedElement(start, exit, radius, nextElement, curvature),
             magneticField_(magneticFieldIntensity * Vector3D::e3) {}
-
+    /**
+     * This constructor takes the mean characteristics of
+     * the particles and calculates the ideal magnetic field.
+     */
+    Dipole(const Vector3D &start, const Vector3D &exit, double radius, Element *nextElement,
+            double curvature, double mass, double charge, double energy) :
+            CurvedElement(start, exit, radius, nextElement, curvature),
+            magneticField_(idealMagneticField(mass, charge, energy)) {}
     const Vector3D magneticForceAt(const Vector3D &) const override { return magneticField_; }
 
     std::ostream &print(std::ostream &os) const override;
